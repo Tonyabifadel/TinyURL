b1d37d457faa99989601320edbbb207978094426
Tony Abi Fadel <tonyabifadel4@gmail.com>
2024-09-18 21:35:29 +0300

    Fixing con for render

--- db.js
+++ db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectMongoose = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect(process.env.DATABASE_URL ,   {
+    serverSelectionTimeoutMS: 30000 // Increase timeout
+   } 
+  );
     console.log("connected to mongoose");
   } catch (error) {
     console.error(error);
